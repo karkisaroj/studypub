5e61fa694cca7b0df48fb7084a4d8eef5e8678e8
Shazam <karkisaroj3012@gmail.com>
2024-07-18 10:17:15 +0545

    User names added

--- studybud/core/views.py
+++ studybud/core/views.py
@@ -1,20 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import Room
+from .forms import RoomForm
 
-rooms= [
-    {'id':1, 'name':'lets learn python'},
-    {'id':2 , 'name':'Design with me'},
-    {'id':3,'name':'Frontend developer'},
-]
+
+# rooms= [
+#     {'id':1, 'name':'lets learn python'},
+#     {'id':2 ,'name':'Design with me'},
+#     {'id':3,'name':'Frontend developer'},
+# ]
 
 def home(request):
+    rooms = Room.objects.all()
     context={'rooms':rooms}
     return render(request,'core/home.html',context )
 
 def room(request,pk):
-    room=None
-    for i in rooms:
-        if i['id']== int(pk):
-            room = i
-        context ={'room':room}
+    room=Room.objects.get(id=pk)
+    context ={'room':room}
 
     return render(request,'core/room.html',context)
+
+def createRoom(request):
+    form=RoomForm()
+    if request.method=='POST':
+        form=RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context={'form':form}
+    return render(request,'core/room_form.html',context)
+
+def updateRoom(request,pk):
+    room=Room.objects.get(id=pk)
+    form=RoomForm(instance=room)
+
+    if request.method=='POST':
+        form=RoomForm(request.POST,instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context={'form':form}
+    return render(request,'core/room_form.html',context)
